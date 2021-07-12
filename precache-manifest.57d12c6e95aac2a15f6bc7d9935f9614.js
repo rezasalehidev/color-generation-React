63f76285e72f1e841b4dc77fb0aea3b88f656400
rezasalehi <rezasalehidev@gmail.com>
2021-07-12 13:35:29 +0430

    Updates

---
+++ precache-manifest.57d12c6e95aac2a15f6bc7d9935f9614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d4f0e99ad055f3e513d4476eda258d",
+    "url": "/color-generation-React/index.html"
+  },
+  {
+    "revision": "bb68be4d99368b6f74ae",
+    "url": "/color-generation-React/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color-generation-React/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generation-React/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb68be4d99368b6f74ae",
+    "url": "/color-generation-React/static/js/main.c22e6a2b.chunk.js"
+  },
+  {
+    "revision": "1fd9f36073fd0dabcd37",
+    "url": "/color-generation-React/static/js/runtime-main.9863c7af.js"
+  }
+]);
